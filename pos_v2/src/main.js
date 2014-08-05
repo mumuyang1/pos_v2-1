d6f9532f970e50d2cc95cf7ec4722b0c91feaa1d
Hao Hu <hhu@thoughtworks.com>
2014-08-05 15:55:36 +0800

    code refactor

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -3,16 +3,13 @@ function printInventory(inputs) {
     var barcodeScanner = new BarcodeScanner();
 
     var barcodes = [];
-
     for (var i = 0; i < inputs.length; i ++) {
         barcodes = barcodes.concat(barcodeScanner.scan(inputs[i]));
     }
 
     var cart = new Cart();
-
     cart.addByBarcodes(barcodes);
 
     var pos = new Pos(cart, new PromotionCalculator());
-
     pos.printInventory(inputs);
 }

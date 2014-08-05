9145f2ccfd1e397362fc5186c3736cf48b65c29d
Hao Hu <hhu@thoughtworks.com>
2014-08-05 17:11:55 +0800

    code refactor

--- pos_v2/src/model/pos.js
+++ pos_v2/src/model/pos.js
@@ -9,7 +9,7 @@ Pos.prototype.printInventory = function () {
         promotionItemsText = '',
         totalPrice = 0,
         promotionTotalPrice = 0,
-        formatPrice = Utils.formatter.formatPrice;
+        formatter = Utils.formatter;
 
     this.promotionCalculator.calculate(this.cart.cartItems);
 
@@ -26,22 +26,22 @@ Pos.prototype.printInventory = function () {
         }
 
         itemsText += '名称：' + item.name + '，数量：' + itemQuantity + item.unit +
-            '，单价：' + formatPrice(item.price) + '(元)，小计：' + formatPrice(subtotal) + '(元)\n';
+            '，单价：' + formatter.formatPrice(item.price) + '(元)，小计：' + formatter.formatPrice(subtotal) + '(元)\n';
 
         totalPrice += subtotal;
     }
 
     console.log(
             '***<没钱赚商店>购物清单***\n' +
-            '打印时间：' + Utils.formatter.formatDateTime(new Date()) + '\n' +
+            '打印时间：' + formatter.formatDateTime(new Date()) + '\n' +
             '----------------------\n' +
             itemsText +
             '----------------------\n' +
             '挥泪赠送商品：\n' +
             promotionItemsText +
             '----------------------\n' +
-            '总计：' + formatPrice(totalPrice) + '(元)\n' +
-            '节省：' + formatPrice(promotionTotalPrice) + '(元)\n' +
+            '总计：' + formatter.formatPrice(totalPrice) + '(元)\n' +
+            '节省：' + formatter.formatPrice(promotionTotalPrice) + '(元)\n' +
             '**********************'
     );
 };
